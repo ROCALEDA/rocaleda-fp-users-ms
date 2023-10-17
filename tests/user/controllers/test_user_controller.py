395a1a0c7982eed8a7b5e69870b63ad8fada4ace
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2023-10-16 19:22:23 -0500

    feat: ajustes creación cliente

--- tests/user/controllers/test_user_controller.py
+++ tests/user/controllers/test_user_controller.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import Mock, AsyncMock
 from app.user.controllers import user_controller
-from app.database.schemas import CandidateCreate
+from app.database.schemas import CandidateCreate, CustomerCreate
 
 
 class TestUserController:
@@ -27,3 +27,24 @@ class TestUserController:
         await create_candidate_func(candidate)
         assert mocked_service.create_candidate.call_count == 1
         mocked_service.create_candidate.assert_called_once_with(candidate)
+    
+    @pytest.mark.asyncio
+    async def test_create_customer(self):
+        mocked_service = Mock()
+        mocked_service.create_customer = AsyncMock()
+
+        create_customer_func = user_controller.initialize(mocked_service)[
+            "create_customer"
+        ]
+
+        customer_data = {
+            "email": "test@example.com",
+            "phone": "+1234567890",
+            "password": "password123",
+            "name": "Compañia XYZ",
+        }
+        customer = CustomerCreate(**customer_data)
+
+        await create_customer_func(customer)
+        assert mocked_service.create_customer.call_count == 1
+        mocked_service.create_customer.assert_called_once_with(customer)
