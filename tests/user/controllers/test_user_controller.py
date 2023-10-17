09d7816aef9d3191a96d5545341fdb5cf54086f6
Camilo Sanchez <camilo.sanchez@heru-app.com>
2023-10-17 16:21:27 -0500

    Add unit tests

--- tests/user/controllers/test_user_controller.py
+++ tests/user/controllers/test_user_controller.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import Mock, AsyncMock
 from app.user.controllers import user_controller
-from app.database.schemas import CandidateCreate, CustomerCreate
+from app.database.schemas import CandidateCreate, CustomerCreate, UserCredentials
 
 
 class TestUserController:
@@ -27,7 +27,7 @@ class TestUserController:
         await create_candidate_func(candidate)
         assert mocked_service.create_candidate.call_count == 1
         mocked_service.create_candidate.assert_called_once_with(candidate)
-    
+
     @pytest.mark.asyncio
     async def test_create_customer(self):
         mocked_service = Mock()
@@ -48,3 +48,19 @@ class TestUserController:
         await create_customer_func(customer)
         assert mocked_service.create_customer.call_count == 1
         mocked_service.create_customer.assert_called_once_with(customer)
+
+    @pytest.mark.asyncio
+    async def test_get_by_credentials(self):
+        mocked_service = Mock()
+        mocked_service.get_by_credentials = AsyncMock()
+
+        get_by_credentials_func = user_controller.initialize(mocked_service)[
+            "get_by_credentials"
+        ]
+
+        credentials_data = {"email": "test@example.com", "password": "password123"}
+        credentials = UserCredentials(**credentials_data)
+
+        await get_by_credentials_func(credentials)
+        assert mocked_service.get_by_credentials.call_count == 1
+        mocked_service.get_by_credentials.assert_called_once_with(credentials)
